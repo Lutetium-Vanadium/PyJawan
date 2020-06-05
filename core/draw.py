26c0413a8a103b6ce70db4959f4d377969a2506f
VrishabKrishna <krishnavrishab@gmail.com>
2020-06-05 15:43:12 +0530

    feat: drawing func headers and grad impl

--- core/draw.py
+++ core/draw.py
@@ -10,10 +10,30 @@ class Drawer:
         return colour
 
     
-    def rect(self, x, y, width, height, colour = [0, 0, 0]):
+    def rect(self, x, y, width, height, colour=(0, 0, 0), thickness=0, fill=True):
         colour = self._regularizeColour(colour)
         self.window.img[y:y+height, x:x+width] = colour
 
-    def square(self, x, y, side, colour = [0, 0, 0]):
-        colour = self._regularizeColour(colour)
-        self.window.img[y:y+side, x:x+side] = colour
+    def line(self, x, y, color=(0, 0, 0), thickness=1):
+        pass
+
+    def ellipse(self, x, y, color=(0, 0, 0), thickness=0, fill=True):
+        pass
+
+    def arc(self, start_pt, stop_pt, start_angle=0, stop_angle=90, thickness=0):
+        pass
+
+    def polygon(self, vertices, thickness=0, fill=True):
+        pass
+
+    def gradient(self, x, y, w, h, color1=(0, 0, 0), color2=(0, 0, 0)):
+        c1 = np.full((1, h, 3), color1, dtype=np.uint8)
+        c2 = np.full((1, h, 3), color2, dtype=np.uint8)
+        base = np.concatenate([c1, c2], axis=0)
+        grad = cv.resize(base, (w, h), cv.INTER_LINEAR)
+        return grad
+
+
+
+    
+    
