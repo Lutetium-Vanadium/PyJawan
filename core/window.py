4681202e91dfff47cae5a0c3629d70a48791724e
ani171202 <anirudh.sathiya@gmail.com>
2020-06-30 11:24:25 +0530

    event handler class created

--- core/window.py
+++ core/window.py
@@ -1,32 +1,38 @@
 import numpy as np
 import cv2 as cv
+from utils.constants import EventType
+
 
 class Window():
     def __init__(self, width, height, name="GUI lib"):
         self.name = name
         self.img = np.zeros((height, width, 3), dtype=np.uint8)
-        self.window = cv.namedWindow(name, cv.WINDOW_NORMAL | cv.WINDOW_GUI_NORMAL)
+        self.window = cv.namedWindow(
+            name, cv.WINDOW_NORMAL | cv.WINDOW_GUI_NORMAL)
         cv.resizeWindow(name, width, height)
 
     @property
     def height(self):
         return self.img.shape[0]
-    
+
     @property
     def width(self):
         return self.img.shape[1]
-    
+
     @property
     def size(self):
         return self.img.shape[1::-1]
-    
+
     def render(self):
         if cv.getWindowProperty(self.name, 4) == 0:
             return True
-        
+
         cv.imshow(self.name, self.img)
 
         return False
 
     def close(self):
         cv.destroyAllWindows()
+
+    def on(self, event_type: EventType):
+        if event_type == EventType.Keypress:
--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,10 @@
+from enum import unique, Enum, auto
+
 FRAME_RATE = 50
+
+
+@unique
+class EventType(Enum):
+    Keypress = auto()
+    MouseMove = auto()
+    MouseClick = auto()
