9775a8281f6e93630e6ac92956455d0c7fc314db
vrishk <krishnavrishab@gmail.com>
2020-09-15 13:17:55 +0530

    chore: test most drawing funcs

--- test.py
+++ test.py
@@ -10,13 +10,27 @@ from utils.constants import FRAME_RATE, EventType
 from utils.color import Color
 
 win = Window(1920, 1080)
-draw = Drawer(win)
+draw = Drawer()
 
 av_time = 0
 c = 0
 
-win.on(EventType.MouseClick, 'test', print)
-win.on(EventType.KeyUp, 'test', lambda evt: print(evt.char))
+
+draw.polygon(win, [[200, 390], [420, 104], [66, 88]], Color(
+    np.random.randint(0, 256**3-1)), thickness=1, fill=True)
+draw.text(win, "Hello, World!", 100, 200, 50,
+          color=Color(np.random.randint(0, 256**3-1)))
+draw.line(win, 0, 0, 50, 50,
+          color=Color(np.random.randint(0, 256**3-1)))
+draw.ellipse(win, 100, 200, 50, 100,
+             color=Color(np.random.randint(0, 256**3-1)), fill=True)
+draw.rect(win, 100, 100, 50, 100,
+          color=Color(np.random.randint(0, 256**3-1)))
+draw.gradient(win, 200, 300, 50, 60,
+              color1=Color(np.random.randint(0, 256**3-1)), color2=Color(np.random.randint(0, 256**3-1)))
+draw.curve(win, [[200, 390], [420, 104], [66, 88]], Color(
+    np.random.randint(0, 256**3-1)), thickness=1, fill=True)
+
 
 while True:
     key = cv.waitKey(100) & 0xFF
@@ -25,9 +39,7 @@ while True:
         break
 
     t = time.time()
-    # draw.polygon([[200, 390], [420, 104], [66, 88]], Color(np.random.randint(0, 256**3-1)), thickness=1, fill=True)
-    draw.text("Hello, World!", 100, 200, 50,
-              color=Color(np.random.randint(0, 256**3-1)))
+
     if not win.render():
         break
 
